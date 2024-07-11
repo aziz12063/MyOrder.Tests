75a5c4c7baf06a903150f5830bc169f85a0e8299
Xsodia <xsodiaz@gmail.com>
2024-07-11 16:24:11 +0200

    Refactor Basket API client and DTOs
    - Removed `BasketApiClient` and its dependencies from `MyOrder.Infrastructure.ApiClients`. This includes removing related using directives like `Microsoft.Extensions.Logging`, `MyOrder.Infrastructure.Repositories`, `MyOrder.Shared.Dtos`, and `Refit`.
    - Updated `BasketAmountsDto` properties in `MyOrder.Shared.Dtos` to use numeric types instead of strings for better precision in mathematical operations. This affects properties such as `AmountWithoutVat`, `DeliveryAmount`, `DiscountAmount`, etc.
    - Modified UI components in `Amounts.razor` to use `RadzenNumeric` for numeric fields, aligning with the changes in `BasketAmountsDto` and enhancing user input accuracy.

--- MyOrder.Shared/Dtos/BasketAmountsDto.cs
+++ MyOrder.Shared/Dtos/BasketAmountsDto.cs
@@ -8,32 +8,32 @@ namespace MyOrder.Shared.Dtos
 {
     public class BasketAmountsDto
     {
-        public string AdditionalSales { get; set; }
-        public string AmountWithoutVat { get; set; }
+        public int AdditionalSales { get; set; }
+        public double AmountWithoutVat { get; set; }
         public string? Availability { get; set; }
-        public string DeliveryAmount { get; set; }
+        public double DeliveryAmount { get; set; }
         public string? DeliveryMarkupCode { get; set; }
         public string? DiffFranco { get; set; }
         public string? DiffKdo { get; set; }
-        public string DiscountAmount { get; set; }
+        public double DiscountAmount { get; set; }
         public string? GiftDescription { get; set; }
         public string? GiftInfo { get; set; }
         public string? InventLocationIds { get; set; }
         public bool IsBasketEmpty { get; set; }
         public bool IsWarrantyAvailable { get; set; }
         public string? MarketingGiftItemId { get; set; }
-        public string OnOrderAmount { get; set; }
+        public double OnOrderAmount { get; set; }
         public string? PackageInsertItemId { get; set; }
-        public string PhysicalAvailableAmount { get; set; }
-        public string ProductsNetAmount { get; set; }
-        public string ProductsVatAmount { get; set; }
+        public double PhysicalAvailableAmount { get; set; }
+        public double ProductsNetAmount { get; set; }
+        public double ProductsVatAmount { get; set; }
         public string? ReceptionDate { get; set; }
-        public string TotalPurchasePrice { get; set; }
-        public string TotalVolume { get; set; }
-        public string TotalWeight { get; set; }
-        public string VatAmount { get; set; }
+        public double TotalPurchasePrice { get; set; }
+        public double TotalVolume { get; set; }
+        public double TotalWeight { get; set; }
+        public double VatAmount { get; set; }
         public List<VatRate> VatRates { get; set; } = new();
-        public string WarrantyAmount { get; set; }
+        public double WarrantyAmount { get; set; }
         public string? WarrantyMarkupCode { get; set; }
         public string? WelcomePackItemId { get; set; }
     }
