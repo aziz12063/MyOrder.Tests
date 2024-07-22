622cf3e53ebf4ae5fe73ecefd702eb9b27476de5
Xsodia <xsodiaz@gmail.com>
2024-07-22 13:31:03 +0200

    Add HoverableMudPaper component for UI enhancement
    - Replaced `<MudPaper>` with `<HoverableMudPaper>` in `Header.razor` for enhanced user interaction on `OrderInfo`, `DeliveryDetails`, `InvoiceDetails`, and `CommercialDetails` components.
    - Introduced `HoverableMudPaper.razor`, a new Razor component that extends functionality with hover effects by adjusting elevation on mouse events.
    - Created `HoverableMudPaperBase.cs`, a base class extending `MudPaper` from MudBlazor, adding hover elevation change capability.
    - Updated `_Imports.razor` with a new using directive for easier access to shared components across the application.
    - Cleaned up `MyOrder.csproj` by removing an unnecessary `<ItemGroup>` reference, streamlining project structure.

---
+++ MyOrder/MyOrder/Components/Childs/Shared/HoverableMudPaperBase.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace MyOrder.Components.Childs.Shared
+{
+    public class HoverableMudPaperBase : MudPaper
+    {
+        [Parameter] public int HoverElevation { get; set; } = 4;
+        protected int currentElevation;
+
+        protected override void OnInitialized()
+        {
+            currentElevation = Elevation;
+        }
+
+        protected void MouseEnterHandler()
+        {
+            currentElevation = HoverElevation;
+        }
+
+        protected void MouseLeaveHandler()
+        {
+            currentElevation = Elevation;
+        }
+    }
+}
