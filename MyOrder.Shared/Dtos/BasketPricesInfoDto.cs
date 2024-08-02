6a959ec2fdb86e82ebc466e317821f670534f468
aziz12063 <aziz12063@gmail.com>
2024-08-02 20:25:22 +0100

    Inplementation of PriceInfo UI

--- MyOrder.Shared/Dtos/BasketPricesInfoDto.cs
+++ MyOrder.Shared/Dtos/BasketPricesInfoDto.cs
@@ -9,32 +9,32 @@ namespace MyOrder.Shared.Dtos
     public class BasketPricesInfoDto
     {
         // Column 1
-        public Field<string> Coupon { get; set; }
-        public Field<decimal?> FreeShippingAmountThreshold { get; set; }
-        public Field<decimal?> GiftAmountThreshold { get; set; }
-        public Field<string> ProductsInfo { get; set; }
+        public Field<string> Coupon { get; set; } = new Field<string> {Value = "coupon" };
+        public Field<decimal> FreeShippingAmountThreshold { get; set; } = new Field<decimal> { Value = 1 };
+        public Field<decimal> GiftAmountThreshold { get; set; } = new Field<decimal> { Value = 1 };
+        public Field<string> ProductsInfo { get; set; } = new Field<string> { Value = "ProdInfo" };
 
         // Column 2
-        public Field<decimal> ProductsNetAmount { get; set; }
-        public Field<string> WarrantyCostOption { get; set; }
-        public Field<decimal> WarrantyCostAmount { get; set; }
-        public Field<string> ShippingCostOption { get; set; }
-        public Field<decimal> ShippingCostAmount { get; set; }
-        public Field<string> LogisticInfo { get; set; }
+        public Field<decimal> ProductsNetAmount { get; set; } = new Field<decimal> { Value = 1 };
+        public Field<string> WarrantyCostOption { get; set; } = new Field<string> { Value = "WarCosOption" };
+        public Field<decimal> WarrantyCostAmount { get; set; } = new Field<decimal> { Value = 1 };
+        public Field<string> ShippingCostOption { get; set; } = new Field<string> { Value = "ShipCoOption" };
+        public Field<decimal> ShippingCostAmount { get; set; } = new Field<decimal> { Value = 1 };
+        public Field<string> LogisticInfo { get; set; } = new Field<string> { Value = "LogiInfo" };
 
         // Column 3
-        public Field<decimal> TotalNetAmount { get; set; }
-        public Field<decimal> VatAmount { get; set; }
-        public Field<decimal> TotalGrossAmount { get; set; }
-        public Field<string> FirstDeliveryDate { get; set; }
-        public Field<string?> FastDeliveryDate { get; set; }
+        public Field<decimal> TotalNetAmount { get; set; } = new Field<decimal> { Value = 1 };
+        public Field<decimal> VatAmount { get; set; } = new Field<decimal> { Value = 1 };
+        public Field<decimal> TotalGrossAmount { get; set; } = new Field<decimal> { Value = 1 };
+        public Field<string> FirstDeliveryDate { get; set; } = new Field<string> { Value = "FirstDelDate" };
+        public Field<string?> FastDeliveryDate { get; set; } = new Field<string> { Value = "FastDelDate" };
 
         // Column 4
-        public Field<int> OrderDiscountRate { get; set; }
-        public Field<bool> OrderLastColumnDiscount { get; set; }
-        public Field<decimal> DdiscountAmount { get; set; }
-        public Field<decimal> AdditionalSalesAmount { get; set; }
-        public Field<decimal> TotalWeight { get; set; }
-        public Field<decimal> TotalVolume { get; set; }
+        public Field<int> OrderDiscountRate { get; set; } = new Field<int> { Value = 1 };
+        public Field<bool> OrderLastColumnDiscount { get; set; } = new Field<bool> { Value = true };
+        public Field<decimal> DdiscountAmount { get; set; } = new Field<decimal> { Value = 1 };
+        public Field<decimal> AdditionalSalesAmount { get; set; } = new Field<decimal> { Value = 1 };
+        public Field<decimal> TotalWeight { get; set; } = new Field<decimal> { Value = 1 };
+        public Field<decimal> TotalVolume { get; set; } = new Field<decimal> { Value = 1 };
     }
 }
---
+++ MyOrder/MyOrder/Components/Childs/Amounts/PriceInfo.razor.cs
@@ -0,0 +1,44 @@
+﻿using MyOrder.Components.Childs.Shared;
+using MyOrder.Shared.Dtos;
+using MyOrder.Store.PricesInfoUseCase;
+
+namespace MyOrder.Components.Childs.Amounts
+{
+    public partial class PriceInfo : BaseFluxorComponent<PricesInfoState, FetchPricesInfoAction>
+    {
+        private BasketPricesInfoDto BasketPricesInfo => State.Value.PricesInfo;
+       
+
+        public string TotalVolumeAndWeight
+        {
+            // Cancatenation of TotalVolume and TotalWeight to string
+             get => BasketPricesInfo.TotalVolume.Value.ToString() + "kg/" + @BasketPricesInfo.TotalWeight.Value.ToString();
+        }
+
+        protected override FetchPricesInfoAction CreateFetchAction(string basketId)
+        {
+            return new FetchPricesInfoAction(basketId);
+        }
+
+
+        private string CouponValue        {
+            get => NullOrWhiteSpaceHelper(BasketPricesInfo.Coupon.Value);
+            set
+            {
+                BasketPricesInfo.Coupon.Value = value;
+                UpdateProcedureCall(value, BasketPricesInfo.Coupon.ProcedureCall);
+            }
+        }
+
+        private static string NullOrWhiteSpaceHelper(string? value) => string.IsNullOrWhiteSpace(value) ? "-" : value;
+        // make it in BaseFluxorComponent, we use it twice 
+        private void UpdateProcedureCall(string? newValue, List<string> procedureCall)
+        {
+            if (procedureCall is { Count: > 0 })
+            {
+                procedureCall[^1] = newValue ?? string.Empty; // Update with the new value or empty string if null
+                OnPropertyUpdatedHandler(procedureCall);
+            }
+        }
+    }
+}
