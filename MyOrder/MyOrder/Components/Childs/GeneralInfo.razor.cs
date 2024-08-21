33f080be6eff9aaa52b00d0a7e6568d67f5a5f8c
Xsodia <xsodiaz@gmail.com>
2024-08-22 00:16:37 +0200

    Refactor basket ID handling and update layout
    - Added null check for `basketId` in `CreateFetchAction` method of `GeneralInfo.razor.cs` to log a warning and return an action with an empty basket ID if `basketId` is null.
    - Added `#warning` directive in `GeneralInfo.razor.cs` for `CacheNewFields` method implementation.
    - Updated `MainLayout.razor` to include additional `@using` directives for `MyOrder.Services` and to inject `NavigationManager` and `BasketService`.
    - Added `OnInitialized` method in `MainLayout.razor` to parse `basketId` from query string and cache it in `BasketService`.
    - Modified `Home.razor` to remove `@page "/Basket/{BasketId}"` directive and replace it with `@page "/"`. Removed `BasketId` parameter and `OnParametersSet` method.
    - Updated `launchSettings.json` to change `launchUrl` from `"Basket/P0130938"` to `"?BasketId=P0130938"` for both HTTP and HTTPS configurations.

--- MyOrder/MyOrder/Components/Childs/GeneralInfo.razor.cs
+++ MyOrder/MyOrder/Components/Childs/GeneralInfo.razor.cs
@@ -8,10 +8,18 @@ public partial class GeneralInfo : BaseFluxorComponent<GeneralInfoState, FetchGe
 {
     protected override FetchGeneralInfoAction CreateFetchAction(GeneralInfoState state, string basketId)
     {
+        if(basketId == null)
+        {
+            Logger.LogWarning("Trying to fetch general info with null basketId at {StackTrace}", LogUtility.GetStackTrace());
+            return new FetchGeneralInfoAction(state, string.Empty);
+        }
         return new FetchGeneralInfoAction(state, basketId);
     }
+
+
+#warning TODO: Should be implemented
     protected override void CacheNewFields()
     {
-        
+         //cache properties in here (state and stufF...)   
     }
 }
