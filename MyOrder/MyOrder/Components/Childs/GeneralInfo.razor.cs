4e0bc4789126311e486984392c3f5914b910b6cd
Xsodia <xsodiaz@gmail.com>
2024-09-02 15:47:55 +0200

    Refactor GeneralInfo and update UI components
    Updated GeneralInfo.razor to handle non-nullable BasketGeneralInfo and added a tooltip for authenticated user info. Introduced BasketGeneralInfo property in GeneralInfo.razor.cs and updated CacheNewFields method. Modified CommercialDetails.razor for better text wrapping and visual separation. Simplified Home.razor by removing authenticated user info section. Changed IBasketRepository DI configuration in Program.cs to use scoped BasketRepository.

--- MyOrder/MyOrder/Components/Childs/GeneralInfo.razor.cs
+++ MyOrder/MyOrder/Components/Childs/GeneralInfo.razor.cs
@@ -1,22 +1,29 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using MyOrder.Components.Common;
+using MyOrder.Shared.Dtos;
 using MyOrder.Store.GeneralInfoUseCase;
+using MyOrder.Utils;
 using System.Security.Claims;
 
 namespace MyOrder.Components.Childs;
 
 public partial class GeneralInfo : BaseFluxorComponent<GeneralInfoState, FetchGeneralInfoAction>
 {
+    private BasketGeneralInfoDto? BasketGeneralInfo { get; set; }
     private ClaimsPrincipal? User { get; set; }
 
-    protected override FetchGeneralInfoAction CreateFetchAction(GeneralInfoState state, string basketId) => new (state, basketId);
+    protected override FetchGeneralInfoAction CreateFetchAction(GeneralInfoState state, string basketId) => new(state, basketId);
 
-
-#warning TODO: Should be implemented
     protected override void CacheNewFields()
     {
-        //User = State.Value.User;
-         //cache properties in here (state and stufF...)   
+        BasketGeneralInfo = State.Value.GeneralInfo
+                             ?? throw new ArgumentNullException(nameof(State.Value.GeneralInfo), "Unexpected null for BasketGeneralInfo object.");
+        User = State.Value.User;
     }
+
+    private string AuthentifiedUser => $"Utilisateur: {FieldUtility.NullOrWhiteSpaceHelperWithDash(User?.Identity?.Name)}"
+        + $"\n Authentifié: {User?.Identity?.IsAuthenticated}"
+        + $"\n Type d'authentification: {FieldUtility.NullOrWhiteSpaceHelperWithDash(User?.Identity?.AuthenticationType)}";
+
 }
