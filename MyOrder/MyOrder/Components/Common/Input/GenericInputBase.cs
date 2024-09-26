def3442750c5ac19b256b9d225578691b3c8e70e
aziz12063 <aziz12063@gmail.com>
2024-09-26 12:15:33 +0100

    Add GenericCheckBox, optimize GenericInputs, implement self refresh call

--- MyOrder/MyOrder/Components/Common/Input/GenericInputBase.cs
+++ MyOrder/MyOrder/Components/Common/Input/GenericInputBase.cs
@@ -1,6 +1,7 @@
 ﻿using Fluxor;
 using Microsoft.AspNetCore.Components;
 using MyOrder.Shared.Dtos.SharedComponents;
+using MyOrder.Store.Base;
 using MyOrder.Store.ProcedureCallUseCase;
 using MyOrder.Utils;
 
@@ -12,15 +13,22 @@ public abstract class GenericInputBase<TValue> : ComponentBase
     protected bool _readWrite;
     protected bool _required;
     protected bool _onlyForDisplay;
+    protected string? _tooltipText;
 
-    [Inject] protected IDispatcher Dispatcher { get; set; }
+    [Inject]
+    protected IDispatcher Dispatcher { get; set; }
+    [CascadingParameter(Name = "FetchActionType")]
+    private Type SelfFetchAction { get; set; }
+    [Parameter]
+    public bool? ReadOnly { get; set; } = null;
+    [Parameter, EditorRequired]
+    public Field<TValue>? Field { get; set; }
 
-    [Parameter] public bool? ReadOnly { get; set; } = null;
     protected TValue? ValueProperty { get; set; }
-    [Parameter, EditorRequired] public Field<TValue>? Field { get; set; }
-    protected void BoundValueChangedHandler() =>
-        Dispatcher.Dispatch(new UpdateFieldAction(Field, ValueProperty));
+    
 
+    protected void BoundValueChangedHandler() =>        
+        Dispatcher.Dispatch(new UpdateFieldAction(Field, ValueProperty, SelfFetchAction));
 
     protected override void OnParametersSet()
     {
@@ -35,6 +43,7 @@ public abstract class GenericInputBase<TValue> : ComponentBase
         _readWrite = FieldUtility.IsReadWrite(Field);
         _required = FieldUtility.IsRequired(Field);
         _onlyForDisplay = FieldUtility.IsOnlyForDisplay(Field);
+        _tooltipText = Field.Description ?? Field.Name;
     }
 
 }
--- MyOrder/MyOrder/Store/ProcedureCallUseCase/ProcedureCallActions.cs
+++ MyOrder/MyOrder/Store/ProcedureCallUseCase/ProcedureCallActions.cs
@@ -3,10 +3,11 @@ using MyOrder.Shared.Dtos.SharedComponents;
 
 namespace MyOrder.Store.ProcedureCallUseCase
 {
-    public class UpdateFieldAction(IField field, dynamic value)
+    public class UpdateFieldAction(IField field, dynamic value, Type type)
     {
         public IField Field { get; } = field;
         public dynamic Value { get; } = value;
+        public Type SelfFetchActionType { get; } = type;
     }
 
     public class PostProcedureCallSuccessAction(string basketId, ProcedureCallResponseDto procedureCallResponse)
