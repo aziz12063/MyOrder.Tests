4ea0655be8c33abb0d38ffd4d99254c2bca84920
aziz12063 <aziz12063@gmail.com>
2024-10-18 11:14:38 +0100

    Implement clipboard support Implement freature to add acticle from OrderedItems, SearchItems to AddLine component

--- MyOrder/MyOrder/Components/Childs/Lines/AddLine/OrderedItemsTab.razor.cs
+++ MyOrder/MyOrder/Components/Childs/Lines/AddLine/OrderedItemsTab.razor.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Components;
 using MyOrder.Components.Common;
+using MyOrder.Services;
 using MyOrder.Shared.Dtos.BasketItems;
+using MyOrder.Shared.Utils;
 using MyOrder.Store.BasketItemsUseCase;
 
 
@@ -8,8 +10,13 @@ namespace MyOrder.Components.Childs.Lines.AddLine;
 
 public partial class OrderedItemsTab : FluxorComponentBase<OrderedItemsState, FetchOrderedItemsAction>, IHandleEvent
 {
-    private List<OrderedItemDto?>? OrderedItems {  get; set; }
+    [Inject]
+    public IClipboardService ClipboardService { get; set; }
+    private List<OrderedItemDto?>? OrderedItems { get; set; }
     private bool _showBlocked = true;
+
+    [Parameter, EditorRequired]
+    public EventCallback<string> ItemClicked { get; set; }
 
     protected override FetchOrderedItemsAction CreateFetchAction(OrderedItemsState state, string basketId) =>
    new(state, basketId);
@@ -25,7 +32,7 @@ public partial class OrderedItemsTab : FluxorComponentBase<OrderedItemsState, Fe
     private void ToggleBlockedCallback(bool applyFilter) =>
         _showBlocked = !applyFilter;
 
-    private bool QuickFilter(BasketItemDto? item)
+    private bool QuickFilter(OrderedItemDto? item)
     {
         if (item == null)
             return false;
@@ -33,6 +40,50 @@ public partial class OrderedItemsTab : FluxorComponentBase<OrderedItemsState, Fe
         // If _showBlocked is true, return true for all items
         // Otherwise, return true only if IsBlocked is false or null (treat null as not blocked)
         return _showBlocked || !(item.IsBlocked ?? false);
+    }
+
+    private void OnItemClick(OrderedItemDto? item)
+    {
+        if (item == null)
+            return;
+
+        ItemClicked.InvokeAsync(item.ItemId);
+    }
+
+    private async Task CopyDataToClipboard()
+    {
+        var headers = new[] {
+                "Date", "Réf. Client", "Code", "Description", "Vendu par",
+                "Quantité", "Prix U", "Remise", "Montant HT", "Statut" };
+
+        //Check wether to include blocked items or not
+        var dataToCopy = _showBlocked ? OrderedItems
+            : OrderedItems?.Where(x => x?.IsBlocked != false).ToList();
+
+        if (dataToCopy is null || dataToCopy.Count == 0)
+        {
+            await ClipboardService.CopyTextToClipboardAsync(string.Empty);
+            return;
+        }
+
+        string formattedData = DataFormatter.GenerateTabSeparatedData(
+            data: dataToCopy,
+            headers: headers,
+            selector: static (item) =>
+            [
+                item?.OrderDate.ToString() ?? string.Empty,
+                item?.CustomerOrderId ?? string.Empty,
+                item?.ItemId ?? string.Empty,
+                item?.Description ?? string.Empty,
+                item?.MultipleQuantity.ToString() ?? string.Empty,
+                item?.Quantity.ToString() ??string.Empty,
+                item?.Price.ToString() ??string.Empty,
+                item?.Discount ??string.Empty,
+                item?.Price.ToString() ??string.Empty,
+                item?.Status ?? string.Empty
+            ]);
+
+        await ClipboardService.CopyTextToClipboardAsync(formattedData);
     }
 
     Task IHandleEvent.HandleEventAsync(
--- MyOrder/MyOrder/Components/Childs/Lines/AddLine/SearchItemsTab.razor.cs
+++ MyOrder/MyOrder/Components/Childs/Lines/AddLine/SearchItemsTab.razor.cs
@@ -1,14 +1,21 @@
 ﻿using Microsoft.AspNetCore.Components;
 using MyOrder.Components.Common;
+using MyOrder.Services;
 using MyOrder.Shared.Dtos.BasketItems;
+using MyOrder.Shared.Utils;
 using MyOrder.Store.BasketItemsUseCase;
 
 namespace MyOrder.Components.Childs.Lines.AddLine;
 
 public partial class SearchItemsTab : FluxorComponentBase<SearchItemsState, FetchSearchItemsAction>, IHandleEvent
 {
+    [Inject]
+    public IClipboardService ClipboardService { get; set; }
     private List<BasketItemDto?>? BasketItems { get; set; }
     private bool _showBlocked = true;
+
+    [Parameter, EditorRequired]
+    public EventCallback<string> ItemClicked { get; set; }
 
     protected override FetchSearchItemsAction CreateFetchAction(SearchItemsState state, string basketId) =>
    new(state, basketId);
@@ -34,6 +41,46 @@ public partial class SearchItemsTab : FluxorComponentBase<SearchItemsState, Fetc
         return _showBlocked || !(item.IsBlocked ?? false);
     }
 
+    private void OnItemClick(BasketItemDto? item)
+    {
+        if (item == null)
+            return;
+
+        ItemClicked.InvokeAsync(item.ItemId);
+    }
+
+    private async Task CopyDataToClipboard()
+    {
+        var headers = new[] {
+            "Code", "Description", "Famille Catalogue", "Groupe",
+            "Sous-groupe", "Vendu par", "Statut" };
+
+        //Check wether to include blocked items or not
+        var dataToCopy = _showBlocked ? BasketItems
+            : BasketItems?.Where(x => x?.IsBlocked != false).ToList();
+
+        if (dataToCopy is null || dataToCopy.Count == 0)
+        {
+            await ClipboardService.CopyTextToClipboardAsync(string.Empty);
+            return;
+        }
+
+        string formattedData = DataFormatter.GenerateTabSeparatedData(
+            data: dataToCopy,
+            headers: headers,
+            selector: static (item) =>
+            [
+                item?.ItemId ?? string.Empty,
+                item?.Description ?? string.Empty,
+                item?.Category1 ?? string.Empty,
+                item?.Category2 ?? string.Empty,
+                item?.Category3 ?? string.Empty,
+                item?.MultipleQuantity.ToString() ?? string.Empty,
+                item?.Status ?? string.Empty
+            ]);
+
+        await ClipboardService.CopyTextToClipboardAsync(formattedData);
+    }
     Task IHandleEvent.HandleEventAsync(
     EventCallbackWorkItem callback, object? arg) =>
     callback.InvokeAsync(arg);
