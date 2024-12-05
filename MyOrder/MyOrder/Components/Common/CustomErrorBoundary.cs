7e9c988935a85eb80d2040226d0dc86494530338
Raja - Imad BOUGATAIA <Imad.BOUGATAIA.ext@raja.fr>
2024-12-05 09:17:22 +0100

    Ajout de CustomErrorBoundary et modifications AddLineTab
    - Ajout d'un espace après l'attribut Disabled="false" dans MudFocusTrap du fichier AddLineTab.razor
    - Suppression des balises de commentaire autour de certains éléments MudItem et ajout de nouveaux éléments MudItem pour inclure des champs supplémentaires comme "Vente add" et "Quantité initiale du client"
    - Ajout du composant CustomErrorBoundary dans CustomErrorBoundary.cs, héritant de ErrorBoundary
    - Injection de dépendances pour ILogger<CustomErrorBoundary> et INotificationService
    - Surcharge de la méthode OnErrorAsync pour enregistrer les erreurs critiques et afficher un message d'erreur via le service de notification

---
+++ MyOrder/MyOrder/Components/Common/CustomErrorBoundary.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using MyOrder.Shared.Enums;
+using MyOrder.Shared.Interfaces;
+
+namespace MyOrder.Components.Common;
+
+public class CustomErrorBoundary : ErrorBoundary
+{
+    [Inject]
+    ILogger<CustomErrorBoundary> Logger { get; set; } = default!;
+    [Inject]
+    INotificationService NotificationService { get; set; } = default!;
+
+    protected override Task OnErrorAsync(Exception ex)
+    {
+        Logger.LogCritical("An unhandled exception occurred.");
+
+        Logger.LogError(ex, "An unhandled exception occurred.");
+
+        NotificationService.ShowMessage("An unexpected error occurred. Please try again later.", NotificationType.Error);
+
+        // Display different messages based on exception type
+        // if (exception is SpecificExceptionType)
+        // {
+        //     NotificationService.ShowMessage("A specific error occurred.", NotificationType.Error);
+        // }
+
+        Recover();
+
+        return Task.CompletedTask;
+    }
+}
